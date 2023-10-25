d6e3b37b01048c0fc5fe12fde36b693c512c06cd
Zachary Weber <zcweber@asu.edu>
2023-10-24 23:11:56 -0700

    ZBranch commit EffortConsoleScene

---
+++ src/application/EffortLoggerConsoleController.java
@@ -0,0 +1,9 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class EffortLoggerConsoleController {
+	public void startActivity(ActionEvent e) {
+		System.out.println("Clicked Start an Activity Button");
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
 module EffortLogger {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
