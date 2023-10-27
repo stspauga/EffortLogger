09bc5c03fb7d44abe1c4f4471e6974732ec3e111
stspauga <116019616+stspauga@users.noreply.github.com>
2023-10-27 11:18:55 -0700

    Revert "Z branch (#2)"
    This reverts commit 59739944ff6cbd9bbd76a92c3e38b629d0f28edb.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,17 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-//import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			primaryStage.setMinHeight(500);
-			primaryStage.setMinWidth(650);
-			Parent root = FXMLLoader.load(getClass().getResource("EffortLoggerConsoleScene.fxml"));
-			//BorderPane root = new BorderPane();
-			Scene scene = new Scene(root);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
