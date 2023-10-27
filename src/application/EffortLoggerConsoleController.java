b9b2bbb8b4eb18e1c88a4475c9f0b62e1def242e
stspauga <116019616+stspauga@users.noreply.github.com>
2023-10-27 11:19:07 -0700

    Revert "Z branch (#2)" (#3)
    This reverts commit 59739944ff6cbd9bbd76a92c3e38b629d0f28edb.

--- src/application/EffortLoggerConsoleController.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import javafx.event.ActionEvent;
-
-public class EffortLoggerConsoleController {
-	public void startActivity(ActionEvent e) {
-		System.out.println("Clicked Start an Activity Button");
-	}
-}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,5 @@
 module EffortLogger {
 	requires javafx.controls;
-	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
