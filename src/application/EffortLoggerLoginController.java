e8d074e0603a9b4d649c16707b6b1349afe46cf9
tpauga <tpauga@asu.edu>
2023-10-28 21:11:12 -0700

    Another

--- src/application/EffortLoggerLoginController.java
+++ src/application/EffortLoggerLoginController.java
@@ -14,10 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import application.UserModel;
-<<<<<<< HEAD
 import application.UserSession;
-=======
->>>>>>> 88d8c26 (Added User Model (#5))
 
 
 public class EffortLoggerLoginController {
@@ -27,18 +24,7 @@ public class EffortLoggerLoginController {
 	
 	
 	public void logIn(ActionEvent e) throws IOException {
-<<<<<<< HEAD
-		
-=======
-		UserModel userModel = new UserModel();
-		// Need to properly validate identity 
-		userModel.createTestUser();
-		// User Authentication is Madeleinne's Prototype task
-		
-	    PasswordField passwordField = null;
-	    String enteredPassword = passwordField.getText();
-	    //detectValidPassword(enteredPassword);
->>>>>>> 88d8c26 (Added User Model (#5))
+
 		if (true) {
 			System.out.println("User Authenticated");
 			// do some things
