06bb6f2cdefa04c3cb0704835a9e89540be33005
Nghiem Nguyen <nghiemnguyen0216@gmail.com>
2023-10-29 13:01:26 -0700

    updated

--- src/application/EffortLoggerLoginController.java
+++ src/application/EffortLoggerLoginController.java
@@ -11,10 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
-import application.UserModel;
-import application.UserSession;
 
 
 public class EffortLoggerLoginController {
