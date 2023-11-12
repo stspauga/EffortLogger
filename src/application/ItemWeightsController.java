0ed8c58cf7412bdfd97fa40474f00a0a8325a5f5
sindhu251001 <sindhu.rallabhandi@gmail.com>
2023-11-12 12:27:05 -0700

    added return buttons to planning poker section

--- src/application/ItemWeightsController.java
+++ src/application/ItemWeightsController.java
@@ -88,4 +88,15 @@ public class ItemWeightsController {
 			exception.printStackTrace();
 		}
 	}
+	
+	//switch to console
+		public void switchToConsole(ActionEvent e) throws IOException {
+			System.out.println("Switching to Console");
+			
+			Parent root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
+			stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
 }
