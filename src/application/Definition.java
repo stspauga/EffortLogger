3de6f5a6012763d02f606a7b4592cb1533ebe615
tpauga <tpauga@asu.edu>
2023-11-11 17:28:40 -0700

    Fixed the definitions section

--- src/application/Definition.java
+++ src/application/Definition.java
@@ -1,17 +1,29 @@
 package application;
 
 public class Definition {
-	private interface definition_item {
-		String PROJ_NAME = "";
-		String DETAIL_ONE = "";
-		String DETAIL_TWO = "";
-		String DETAIL_THREE = "";
-		String LIFE_CYCLE = "";
+	String PROJ_NAME;
+	String DETAILS;
+	String LIFE_CYCLE;
+   
+
+
+    Definition setDefinitionItem(String proj_name, String details, String life_cycle) {
+    	this.PROJ_NAME = proj_name;
+    	this.DETAILS = details;
+    	this.LIFE_CYCLE = life_cycle;
+    	return this;
+    }
+    
+    public String getPROJ_NAME() {
+        return PROJ_NAME;
+    }
+    
+    public String getDETAILS() {
+    	return DETAILS;
+    }
+
+	public String getLIFE_CYCLE() {
+		return LIFE_CYCLE;
 	}
-	
-	private static definition_item[] definitionItemsArray;
-	
-	private void searchForProject(String projName) {
-		
-	}
+
 }
