8251acd419d638a634a2e4cc43286bd3558a3ce2
stspauga <116019616+stspauga@users.noreply.github.com>
2023-11-12 16:29:36 -0700

    Nghiem branch1 (#21)
    * message
    
    * new update
    
    * new update
    
    * message
    
    * message
    
    * message
    
    * message
    
    * merge
    
    ---------
    
    Co-authored-by: Nghiem Nguyen <nghiemnguyen0216@gmail.com>

--- src/application/EffortLoggerConsoleController.java
+++ src/application/EffortLoggerConsoleController.java
@@ -134,13 +134,13 @@ public class EffortLoggerConsoleController {
 	}
 
 	// Switch to the Effort Logger Editor Scene 
-	public void switchToLoginPage(ActionEvent e) throws IOException {
-		closeTutorial();
-		System.out.println("Switching to Login Page");
-		String newScreenFile = "EffortLoggerLogin.fxml";
-		switchScreen(newScreenFile, e);
-		
-	}
+		public void switchToLoginPage(ActionEvent e) throws IOException {
+			closeTutorial();
+			System.out.println("Switching to Login Page");
+			String newScreenFile = "EffortLoggerLogin.fxml";
+			switchScreen(newScreenFile, e);
+			
+		}
 	
 	//Switch to the Planning poker scene
 	public void switchToPlanningPoker(ActionEvent e) throws IOException {
