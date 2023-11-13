04ee3ed0c986341c3889e11fe8f3579b185ffaf6
stspauga <116019616+stspauga@users.noreply.github.com>
2023-11-12 19:04:01 -0700

    Madie tan branch (#22)
    * Made commit for updated Password checker class
    
    * 10.28.2023 9pm Commit Madie Tan
    
    * newest commit
    
    * I sort of fixed it!!
    
    * Before merging to master at 4:35pm
    
    * Commit after making SettingsConsole.fxml
    
    * Commit changes for Settings
    
    ---------
    
    Co-authored-by: MDTea <90730369+MDTea@users.noreply.github.com>

--- src/application/SettingsConsoleController.java
+++ src/application/SettingsConsoleController.java
@@ -54,7 +54,7 @@ public class SettingsConsoleController {
     private TextField tfState;
     @FXML
     private TextField tfZip;
-;
+
     @FXML
     private Text zipQ;
 
@@ -79,6 +79,7 @@ public class SettingsConsoleController {
     }
     
   	//get user data from settings, store them in UserData.java
+
   	//UserData(String firstName, String LastName, String username, String password, String email)
   	public void collectUserData(ActionEvent e) throws IOException{
   		String firstName = tfFirstName.getText();
@@ -91,4 +92,5 @@ public class SettingsConsoleController {
   		
   		
   	}
+
 }
