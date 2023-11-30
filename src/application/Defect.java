184bda6933b8e9889417b77642f119bfc7495e69
Zachary Weber <zcweber@asu.edu>
2023-11-30 15:03:00 -0700

    pre-mm

--- src/application/Defect.java
+++ src/application/Defect.java
@@ -10,12 +10,57 @@ public class Defect {
 	private String category;
 	
 	public Defect(int id, String startStep) {
-		this.id = id;
-		name = "new-log-" + Integer.toString(id);
-		status = true;
-		description = "";
+		this.setId(id);
+		setName("new-log-" + Integer.toString(id));
+		setStatus(true);
+		setDescription("");
 		startStep = null;
-		endStep = null;
-		category = "Not Specified";
+		setEndStep(null);
+		setCategory("Not Specified");
 	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStartStep() {
+		return startStep;
+	}
+	public void setStartStep(String startStep) {
+		this.startStep = startStep;
+	}
+	public String getEndStep() {
+		return endStep;
+	}
+	public void setEndStep(String endStep) {
+		this.endStep = endStep;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
 }
