eec6f346e0ed0408ead8205a0286ee741a7da8b4
stspauga <116019616+stspauga@users.noreply.github.com>
2023-11-30 21:07:49 -0700

    Madie tan branch (#32)
    * Made commit for updated Password checker class
    
    * 10.28.2023 9pm Commit Madie Tan
    
    * newest commit
    
    * I sort of fixed it!!
    
    * Before merging to master at 4:35pm
    
    * Commit after making SettingsConsole.fxml
    
    * Commit changes for Settings
    
    * Commit - some new updates with UserData and EffortLoggerLoginController
    
    * Made some changes to UserData, SettingsConsoleController
    
    * Commit before pulling from master
    
    * Commit after pulling from master
    
    * Commit before pulling from master
    
    * Commit before pulling from master
    
    * Fixed prototype
    
    * Commit: updated code for individual assignment
    
    * Commit because I wanna save changes about this scene builder in PlanningPokerCards.fxml before I go trippy with them later on
    
    * Commit before merging from master
    
    * New commit to check PlanningPokerCardController
    
    * Commit Planning Poker Cards -- updated and it works!
    
    ---------
    
    Co-authored-by: MDTea <90730369+MDTea@users.noreply.github.com>
    Co-authored-by: tpauga <tpauga@asu.edu>

--- src/application/CardDisplayController.java
+++ src/application/CardDisplayController.java
@@ -29,37 +29,35 @@ public class CardDisplayController {
 	
 	public void displayCard(String userStory, String itemName, int weight, String criteria, ActionEvent e)
 	{
-		
 		itemNameLabel.setText(itemName);
 		userStoryLabel.setText(userStory);
 		assignedWeightLabel.setText(Integer.toString(weight));
 		criteriaTextArea.setText(criteria);
-		
 	}
 	
 	//switch to console
-			public void switchToConsole(ActionEvent e) throws IOException {
-				System.out.println("Switching to Console");
-				Parent root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
-				stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-				scene = new Scene(root);
-				stage.setScene(scene);
-				stage.show();
-			}
-			//Switch to the Planning poker scene
-			public void switchToPlanningPoker(ActionEvent e) throws IOException {
-				System.out.println("Switching to Planning Poker");
-				String newScreenFile = "PlanningPoker.fxml";
-				switchScreen(newScreenFile, e);
-			}
-			
-			
-			public void switchScreen (String newScreenFile, ActionEvent e) throws IOException{
-				Parent root = FXMLLoader.load(getClass().getResource(newScreenFile));
-				stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-				scene = new Scene(root);
-				stage.setScene(scene);
-				stage.show();
-			}
+	public void switchToConsole(ActionEvent e) throws IOException {
+		System.out.println("Switching to Console");
+		Parent root = FXMLLoader.load(getClass().getResource("EffortLoggerConsole.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	//Switch to the Planning poker scene
+	public void switchToPlanningPoker(ActionEvent e) throws IOException {
+		System.out.println("Switching to Planning Poker");
+		String newScreenFile = "PlanningPoker.fxml";
+		switchScreen(newScreenFile, e);
+	}
+	
+	
+	public void switchScreen (String newScreenFile, ActionEvent e) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource(newScreenFile));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
